fe4bf57a09f14e586e273f4726328132721e8998
m_shabne <a.shabane@gmail.com>
2021-02-07 17:17:18 +0330

    testing argsparse was faild but we commit it for time that we need some thing

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #!/bin/python3
+import nd
+import pld
+import argparse
+
+# Description's
+play_list_des = "If video have play list find and download all playlist. if play list url given, download all play list"
+q_des = "144, 360, 480, 720, 1080"
+spider_des = "If --spider or -s specify, it will crawl the page given and download all the play list"
+just_list_des = "Just print the video's link in stdout"
+
+# Craate Commad line Option 
+parser = argparse.ArgumentParser(description = "Any Aparat downloader such as Playlist Or Normal video")
+parser.add_argument("link", help="Page link")
+parser.add_argument("--quality", "-q", help=q_des, default="480")
+parser.add_argument("--find-playlist", "-p", help = play_list_des)
+parser.add_argument("--spider", "-s", help = spider_des)
+parser.add_argument("--just-list", "-l", help = just_list_des)
+
+args = parser.parse_args()
+link = args.link
+quality = args.quality
+spider = args.spider
+just_link = args.justlist
