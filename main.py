3316ca6cc88f162a4aecce1e76e434fc1c93bee3
m_shabne <a.shabane@gmail.com>
2021-02-07 20:42:29 +0330

    command line user interface was created with flowing option: --quality, --playlist, --spider, --just-list

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import nd
 import pld
 import argparse
 
-# Description's
+
+# Description for each option and paramer
 play_list_des = "If video have play list find and download all playlist. if play list url given, download all play list"
 q_des = "144, 360, 480, 720, 1080"
 spider_des = "If --spider or -s specify, it will crawl the page given and download all the play list"
 just_list_des = "Just print the video's link in stdout"
+primary_des = "Any Aparat downloader such as Playlist Or Normal video"
 
-# Craate Commad line Option 
-parser = argparse.ArgumentParser(description = "Any Aparat downloader such as Playlist Or Normal video")
+# Craate Commad line interface
+parser = argparse.ArgumentParser(prog="apd", description=primary_des)
 parser.add_argument("link", help="Page link")
-parser.add_argument("--quality", "-q", help=q_des, default="480")
-parser.add_argument("--find-playlist", "-p", help = play_list_des)
-parser.add_argument("--spider", "-s", help = spider_des)
-parser.add_argument("--just-list", "-l", help = just_list_des)
+parser.add_argument("-q", "--quality", help=q_des, default="480")
+parser.add_argument("-p", "--playlist", help=play_list_des, action="store_true")
+parser.add_argument("-s", "--spider", help=spider_des, action="store_true")
+parser.add_argument("-l", "--just-list", help=just_list_des, action="store_true")
 
+# Set the variable
 args = parser.parse_args()
 link = args.link
 quality = args.quality
-spider = args.spider
-just_link = args.justlist
+playlist = args.playlist
+spider = args.spider    
+just_list = args.just_list
+
+
